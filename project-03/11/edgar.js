13ed588ee22c72c3a3b81da5f34e1a74e5981177
sbolsec <sbolsec@croz.net>
2022-01-08 00:31:41 +0100

    11 zad

--- project-03/11/edgar.js
+++ project-03/11/edgar.js
@@ -1,35 +1,12 @@
-// Upit koji vraca broj proizvoda koji zadovoljavaju uvjete.
-// Trajanje upita: 0.109 sekunda
-db.products.aggregate([{ 
-    $match: {
-        $and: [
-            { "reviewStats.reviewCount": { $gte: 200 } },
-            { "reviewStats.avgReviewScore": { $gte: 4.5 } },
-            { "price": { $lt: 20 } },
-            { $or: [
-                { "reviews": { $elemMatch: { "title": { $regex: "wonderful" } } } },
-                { "reviews": { $elemMatch: { "summary": { $regex: "wonderful" } } } },
-                { "reviews": { $elemMatch: { "text": { $regex: "wonderful" } } } },
-            ]}
-        ]
-    } 
-}, { $count: "passing" }
-]);
+// Trajanje upita: 0.114 sekunda
 
-// Upit koji vraca proizvode koji zadovoljavaju uvjete.
-// Trajanje upita: 0.119 sekunda
-db.products.aggregate([{ 
-    $match: {
-        $and: [
-            { "reviewStats.reviewCount": { $gte: 200 } },
-            { "reviewStats.avgReviewScore": { $gte: 4.5 } },
-            { "price": { $lt: 20 } },
-            { $or: [
-                { "reviews": { $elemMatch: { "title": { $regex: "wonderful" } } } },
-                { "reviews": { $elemMatch: { "summary": { $regex: "wonderful" } } } },
-                { "reviews": { $elemMatch: { "text": { $regex: "wonderful" } } } },
-            ]}
-        ]
-    } 
-}
-]);
+db.products.find({
+    "reviewStats.reviewCount": { $gte: 200 },
+    "reviewStats.avgReviewScore": { $gte: 4.5 },
+    "price": { $lt: 20 },
+    $or: [
+        { "reviews": { $elemMatch: { "title": { $regex: "wonderful" } } } },
+        { "reviews": { $elemMatch: { "summary": { $regex: "wonderful" } } } },
+        { "reviews": { $elemMatch: { "text": { $regex: "wonderful" } } } },
+    ]
+}).count();

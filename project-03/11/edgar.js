0b199b2ec21238a80764e929cb422954b2a094cd
sbolsec <sbolsec@croz.net>
2022-01-08 00:24:30 +0100

    11 zad

--- project-03/11/edgar.js
+++ project-03/11/edgar.js
@@ -1,2 +1,35 @@
-// Trajanje upita:  sekunda
+// Upit koji vraca broj proizvoda koji zadovoljavaju uvjete.
+// Trajanje upita: 0.109 sekunda
+db.products.aggregate([{ 
+    $match: {
+        $and: [
+            { "reviewStats.reviewCount": { $gte: 200 } },
+            { "reviewStats.avgReviewScore": { $gte: 4.5 } },
+            { "price": { $lt: 20 } },
+            { $or: [
+                { "reviews": { $elemMatch: { "title": { $regex: "wonderful" } } } },
+                { "reviews": { $elemMatch: { "summary": { $regex: "wonderful" } } } },
+                { "reviews": { $elemMatch: { "text": { $regex: "wonderful" } } } },
+            ]}
+        ]
+    } 
+}, { $count: "passing" }
+]);
 
+// Upit koji vraca proizvode koji zadovoljavaju uvjete.
+// Trajanje upita: 0.119 sekunda
+db.products.aggregate([{ 
+    $match: {
+        $and: [
+            { "reviewStats.reviewCount": { $gte: 200 } },
+            { "reviewStats.avgReviewScore": { $gte: 4.5 } },
+            { "price": { $lt: 20 } },
+            { $or: [
+                { "reviews": { $elemMatch: { "title": { $regex: "wonderful" } } } },
+                { "reviews": { $elemMatch: { "summary": { $regex: "wonderful" } } } },
+                { "reviews": { $elemMatch: { "text": { $regex: "wonderful" } } } },
+            ]}
+        ]
+    } 
+}
+]);

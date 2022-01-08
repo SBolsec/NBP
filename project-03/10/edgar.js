2dabb80d2d41c56332be740d1e6e28568decfeb1
sbolsec <sbolsec@croz.net>
2022-01-08 01:45:04 +0100

    11 zad i pol 10.

--- project-03/10/edgar.js
+++ project-03/10/edgar.js
@@ -1,2 +1,54 @@
-// Trajanje upita:  sekunda
+// Trajanje upita sa regexom: 0.136 sekunda
+// Trajanje upita sa indexom:  sekunda
 
+// Upit sa regexom
+db.watches()
+
+db.products.aggregate([{
+    $match: {
+        $or: [
+            { "reviews": { $elemMatch: { "title": { $regex: "mindful" } } } },
+            { "reviews": { $elemMatch: { "summary": { $regex: "mindful" } } } },
+            { "reviews": { $elemMatch: { "text": { $regex: "mindful" } } } },
+        ]
+    }
+},{ $project: {
+        reviews: { 
+            $filter: {
+                input: "$reviews",
+                cond: { 
+                    $or: [
+                        { $regexMatch: { input: "$$this.summary", regex: "mindful" } },
+                        { $regexMatch: { input: "$$this.text", regex: "mindful" } }
+                    ]
+                }
+            }
+        }
+    }
+}
+]);
+
+
+// Kreiranje indeksa
+// Trajanje upita: 4.07 sekunda
+db.products.createIndex({ "reviews.summary": "text", "reviews.text": "text" });
+
+
+// Upit sa indeksom
+db.products.aggregate([{
+    $unwind: "reviews"
+},
+{
+    $match: { $text: { $search: "wonderful" } }
+},{ $project: {
+        reviews: { 
+            $filter: {
+                input: "$reviews",
+                cond: { 
+                    $text: { $search: "wonderful" } 
+                }
+            }
+        }
+    }
+}
+]);
--- project-03/11/edgar.js
+++ project-03/11/edgar.js
@@ -1,12 +1,8 @@
-// Trajanje upita: 0.114 sekunda
+// Trajanje upita: 0.017 sekunda
 
 db.products.find({
     "reviewStats.reviewCount": { $gte: 200 },
     "reviewStats.avgReviewScore": { $gte: 4.5 },
     "price": { $lt: 20 },
-    $or: [
-        { "reviews": { $elemMatch: { "title": { $regex: "wonderful" } } } },
-        { "reviews": { $elemMatch: { "summary": { $regex: "wonderful" } } } },
-        { "reviews": { $elemMatch: { "text": { $regex: "wonderful" } } } },
-    ]
+    $text: { $search: "wonderful" },
 }).count();

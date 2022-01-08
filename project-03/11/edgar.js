738eb947625054faf8f638ba7470b1e2b8d3008f
sbolsec <sbolsec@croz.net>
2022-01-08 14:36:03 +0100

    11 zad

--- project-03/11/edgar.js
+++ project-03/11/edgar.js
@@ -1,5 +1,9 @@
 // Trajanje upita: 0.017 sekunda
 
+// Priprema indeksa za brzu pretragu
+// Trajanje upita: 4.07 sekunda
+db.products.createIndex({ "review.summary": "text", "review.text": "text" });
+
 db.products.find({
     "reviewStats.reviewCount": { $gte: 200 },
     "reviewStats.avgReviewScore": { $gte: 4.5 },
